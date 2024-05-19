ef0854bb3beba9c22f695c206e870d92e958b7fc
Shannon Hanneman <taylor.shannon.n@gmail.com>
2024-05-19 16:10:08 -0500

    Adding basic login test for SwagLabs demo page, set up tsconfig

---
+++ pages/login.ts
@@ -0,0 +1,29 @@
+import {expect, type Page, type Locator } from '@playwright/test';
+
+export class LoginPage {
+
+    readonly page: Page;
+    readonly loginURL: string;
+    readonly usernameField: Locator;
+    readonly passwordField: Locator;
+    readonly loginButton: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.loginURL = "https://www.saucedemo.com";
+        this.usernameField = page.getByTestId('username');
+        this.passwordField = page.getByTestId('password');
+        this.loginButton = page.getByTestId('login-button');
+    }
+
+    async gotoLogin() {
+        await this.page.goto(this.loginURL);
+    }
+
+    async login(username: string, password: string) {
+        await this.usernameField.fill(username);
+        await this.passwordField.fill(password);
+        await this.loginButton.click();
+    }
+    
+}
---
+++ tests/swaglabs/login.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../../pages/login';
+
+const STANDARD_USER_ID = "standard_user";
+const LOCKED_OUT_USER_ID = "locked_out_user"
+const SAUCE_USER_PASSWORD = "secret_sauce";
+
+
+test.describe("Login to Swag Labs" , () => { 
+    test('standard user login', async ({ page }) => {
+        const loginPage = new LoginPage(page); 
+        await loginPage.gotoLogin();
+        await loginPage.login(STANDARD_USER_ID, SAUCE_USER_PASSWORD);
+        
+        // Expect page navigation after successful login
+        await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+    });
+
+    test('locked out user login', async ({page}) => {
+        const loginPage = new LoginPage(page);
+        await loginPage.gotoLogin();
+        await loginPage.login(LOCKED_OUT_USER_ID, SAUCE_USER_PASSWORD);
+
+        //Expect error reporting user has been locked out
+        await expect(page.getByText('Epic sadface: Sorry, this user has been locked out.')).toBeVisible();
+    })
+});
