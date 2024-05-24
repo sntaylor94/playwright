3f3c0287cf4aee9a325faa278377e190c121ce3b
Shannon Hanneman <taylor.shannon.n@gmail.com>
2024-05-24 13:24:57 -0500

    Removing credentials from spec code

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -3,9 +3,6 @@ import { LoginPage } from '../pages/login';
 
 const authFile = '../../.auth/user.json'
 
-const STANDARD_USER_ID = "standard_user";
-const SAUCE_USER_PASSWORD = "secret_sauce";
-
 setup('authenticate user', async({ page }) => {
     const loginPage = new LoginPage(page);
     await loginPage.gotoLogin();
